95ad902e289af00d8ca4cbc8adc9d4e0186bb8b6
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-20 00:18:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,5 +12,5 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(kids)
-  birthday_kids.each{|name, age| if age =< 12 puts "Happy Birthday #{kids_name}! You are now #{age} years old!"}
+  birthday_kids.each{|name, age| if age <= 12 puts "Happy Birthday #{kids_name}! You are now #{age} years old!"}
 end
